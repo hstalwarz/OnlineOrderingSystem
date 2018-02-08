fc5480704a05e8701cea8929742480c641003869
Harkar <harkar.talwar@gmail.com>
2018-02-08 15:45:18 -0800

    Updating DBDump file to include creation of schema

--- DBDump.sql
+++ DBDump.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `online_ordering_system` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `online_ordering_system`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: online_ordering_system
@@ -108,4 +110,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-02-08 14:50:54
+-- Dump completed on 2018-02-08 15:43:34
